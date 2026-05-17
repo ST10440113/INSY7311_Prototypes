f4c0186f79d4c309cebda62afceca099e92fba6a
diya singh <161337050+ST10440113@users.noreply.github.com>
2026-05-17 20:36:25 +0200

    added home page

--- app/src/main/java/com/example/insy7311_prototypes/WellnessResources.kt
+++ app/src/main/java/com/example/insy7311_prototypes/WellnessResources.kt
@@ -1,33 +1,16 @@
 package com.example.insy7311_prototypes
 
-import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.TextView
-import android.widget.Button
-import android.content.Intent
 
-class Questions : AppCompatActivity() {
-
-    private lateinit var textViewQuestionHeading: TextView
-
-    private lateinit var buttonContinue :Button
-
+class WellnessResources : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_questions)
-        textViewQuestionHeading = findViewById(R.id.textViewQuestionHeading)
-        buttonContinue = findViewById(R.id.buttonContinue)
-
-        textViewQuestionHeading.paintFlags = Paint.UNDERLINE_TEXT_FLAG
-
-        buttonContinue.setOnClickListener {
-            startActivity(Intent(this,Home::class.java))
-        }
+        setContentView(R.layout.activity_wellness_resources)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
