36130f8914657001bf79311425626c5a45331679
diya singh <161337050+ST10440113@users.noreply.github.com>
2026-05-28 21:27:27 +0200

    completed wellness page

--- app/src/main/java/com/example/insy7311_prototypes/WellnessResources.kt
+++ app/src/main/java/com/example/insy7311_prototypes/WellnessResources.kt
@@ -1,5 +1,6 @@
 package com.example.insy7311_prototypes
 
+import android.content.Intent
 import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -10,15 +11,21 @@ import android.widget.TextView
 
 class WellnessResources : AppCompatActivity() {
     private lateinit var textViewWellnessHeading : TextView
+
+    private lateinit var textViewBreadHome : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_wellness_resources)
 
         textViewWellnessHeading = findViewById(R.id.textViewWellnessHeading)
-
+        textViewBreadHome = findViewById(R.id.textViewBreadHome)
 
         textViewWellnessHeading.paintFlags = Paint.UNDERLINE_TEXT_FLAG
+
+        textViewBreadHome.setOnClickListener {
+            startActivity(Intent(this, Home::class.java))
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
